d8b3b06ce4829952711dc1acedae4ad225aef79f
Yamatchi <ussousso12@gmail.com>
2025-05-07 20:58:12 +0900

    feat : 테스트용 스크립트 추가
    테스트용 스크립트 추가

---
+++ Assets/Scripts/text_log.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+//public class Dialogue
+//{
+//    [TextArea]
+//    public string dialogue;
+//}
+public class text_log_Base : MonoBehaviour
+{
+    // 
+    public int npc_talk_int = 0;
+}
