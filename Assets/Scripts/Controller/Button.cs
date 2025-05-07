533aa629e0fe7183417fba340529ed971ef686e8
Yamatchi <ussousso12@gmail.com>
2025-05-07 15:54:45 +0900

    feat : player 위치 저장 코드 추가
    player 위치 저장 코드 추가

--- Assets/Scripts/Controller/Button.cs
+++ Assets/Scripts/Controller/Button.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    void NpcTalkYes()
+    Vector3 GameArea1 = new Vector3(-0.5f, 8, 0);
+    public void NpcTalkYes()
     {
-        GameManager.Instance.player.playerLastPos = Vector3.zero;
+        GameManager.Instance.player.playerLastPos = GameManager.Instance.player.transform.position;
+        GameManager.Instance.player.transform.position = GameArea1;
     }
 }
